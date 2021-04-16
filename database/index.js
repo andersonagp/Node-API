5b5820d9617a61031f6a4abfa5d56e12b38c11a3
Anderson Gonçalves <andersonconq@gmail.com>
2021-04-16 15:34:17 +0100

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://anderson:102030@mycluster-h7epj.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://');
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
